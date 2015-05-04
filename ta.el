8a51387e5377e6e8a541d062a35c8587403f6a06
kuanyui <azazabc123@gmail.com>
2015-05-04 22:16:28 +0800

    ˊ・ω・ˋ

--- ta.el
+++ ta.el
@@ -3,20 +3,18 @@
 ;; Copyright (C) 2015  kuanyui
 
 ;; Author: kuanyui <azazabc123@gmail.com>
+;; X-URL: http://github.com/kuanyui/ta.el
+;; Version: 1.0
 ;; Keywords: tools
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; WTFPL 2.0
+;; Ono Hiroko (kuanyui) (ɔ) Copyleft 2014
+;;
+;; This program is free software. It comes without any warranty, to
+;; the extent permitted by applicable law. You can redistribute it
+;; and/or modify it under the terms of the Do What The Fuck You Want
+;; To Public License, Version 2, as published by Sam Hocevar. See
+;; http://www.wtfpl.net/ for more details.
 
 ;;; Commentary:
 ;;
