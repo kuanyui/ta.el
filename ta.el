5c185529dd557a8acdc342a56c1de13b9248e352
kuanyui <azazabc123@gmail.com>
2015-05-04 21:48:18 +0800

    Update README.

--- ta.el
+++ ta.el
@@ -19,7 +19,10 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
+;;
+;; - M-x `ta-mode' to activate, then:
+;;   + Use `M-p' and `M-n' to modify the homophonic characters.
+;;   + Use `M-i' and `M-o' to jump between all possible candidate characters.
 ;;
 
 ;;; Code:
